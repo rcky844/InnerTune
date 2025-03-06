f0f296d44ff59165ad67bec916369118d38e24be
Davide Garberi <dade.garberi@gmail.com>
2025-03-06 21:34:17 +0100

    app: ui: LoginScreen: Extract the dataSyncId properly
     * Before we were possibly getting multiple Ids chained
       together with "||".

--- app/src/main/java/com/zionhuang/music/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/zionhuang/music/ui/screens/LoginScreen.kt
@@ -95,7 +95,7 @@ fun LoginScreen(
                     @JavascriptInterface
                     fun onRetrieveDataSyncId(newDataSyncId: String?) {
                         if (newDataSyncId != null) {
-                            dataSyncId = newDataSyncId
+                            dataSyncId = newDataSyncId.substringBefore("||")
                         }
                     }
                 }, "Android")
