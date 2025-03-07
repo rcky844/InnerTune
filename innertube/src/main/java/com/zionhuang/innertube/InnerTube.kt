26187140df478538105bd18d7628b7049a99b37e
Davide Garberi <dade.garberi@gmail.com>
2025-03-07 01:17:01 +0100

    innertube: Avoid using dataSyncId when use login is disabled

--- innertube/src/main/java/com/zionhuang/innertube/InnerTube.kt
+++ innertube/src/main/java/com/zionhuang/innertube/InnerTube.kt
@@ -110,7 +110,11 @@ class InnerTube {
         ytClient(client, setLogin = useLoginForBrowse)
         setBody(
             SearchBody(
-                context = client.toContext(locale, visitorData, dataSyncId),
+                context = client.toContext(
+                    locale,
+                    visitorData,
+                    if (useLoginForBrowse) dataSyncId else null
+                ),
                 query = query,
                 params = params
             )
@@ -164,7 +168,11 @@ class InnerTube {
         ytClient(client, setLogin = setLogin || useLoginForBrowse)
         setBody(
             BrowseBody(
-                context = client.toContext(locale, visitorData, dataSyncId),
+                context = client.toContext(
+                    locale,
+                    visitorData,
+                    if (setLogin || useLoginForBrowse) dataSyncId else null
+                ),
                 browseId = browseId,
                 params = params
             )
