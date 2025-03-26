reocat/OuterTune

potoken, player: improve error handling and make client fallback work if session identifier is null

app/src/main/java/com/dd3boh/outertune/utils/YTPlayerUtils.kt
@@ -5,6 +5,7 @@ import androidx.media3.common.PlaybackException
 import com.dd3boh.outertune.constants.AudioQuality
 import com.dd3boh.outertune.db.entities.FormatEntity
 import com.dd3boh.outertune.utils.potoken.PoTokenGenerator
+import com.dd3boh.outertune.utils.potoken.PoTokenResult
 import com.zionhuang.innertube.NewPipeUtils
 import com.zionhuang.innertube.YouTube
 import com.zionhuang.innertube.models.YouTubeClient
@@ -69,7 +70,7 @@ object YTPlayerUtils {
          */
         val signatureTimestamp = getSignatureTimestampOrNull(videoId)
 
-        val (webPlayerPot, webStreamingPot) = poTokenGenerator.getWebClientPoToken(videoId)?.let {
+        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId)?.let {
             Pair(it.playerRequestPoToken, it.streamingDataPoToken)
         } ?: Pair(null, null)
 
@@ -125,7 +126,8 @@ object YTPlayerUtils {
                         connectivityManager,
                     ) ?: continue
                 streamUrl = findUrlOrNull(format, videoId) ?: continue
-                streamExpiresInSeconds = streamPlayerResponse.streamingData?.expiresInSeconds ?: continue
+                streamExpiresInSeconds =
+                    streamPlayerResponse.streamingData?.expiresInSeconds ?: continue
 
                 if (client.useWebPoTokens && webStreamingPot != null) {
                     streamUrl += "&pot=$webStreamingPot";
@@ -245,5 +247,17 @@ object YTPlayerUtils {
                 reportException(it)
             }
             .getOrNull()
+    }
+
+    /**
+     * Wrapper around the [PoTokenGenerator.getWebClientPoToken] function which reports exceptions
+     */
+    private fun getWebClientPoTokenOrNull(videoId: String): PoTokenResult? {
+        try {
+            return poTokenGenerator.getWebClientPoToken(videoId)
+        } catch (e: Exception) {
+            reportException(e)
+        }
+        return null
     }
 }

rcky844/InnerTune

app/src/main/java/com/zionhuang/music/utils/YTPlayerUtils.kt
@@ -12,6 +12,7 @@ import com.zionhuang.innertube.models.response.PlayerResponse
 import com.zionhuang.music.constants.AudioQuality
 import com.zionhuang.music.db.entities.FormatEntity
 import com.zionhuang.music.utils.potoken.PoTokenGenerator
+import com.zionhuang.music.utils.potoken.PoTokenResult
 import okhttp3.OkHttpClient
 
 object YTPlayerUtils {
@@ -69,7 +70,7 @@ object YTPlayerUtils {
          */
         val signatureTimestamp = getSignatureTimestampOrNull(videoId)
 
-        val (webPlayerPot, webStreamingPot) = poTokenGenerator.getWebClientPoToken(videoId)?.let {
+        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId)?.let {
             Pair(it.playerRequestPoToken, it.streamingDataPoToken)
         } ?: Pair(null, null)
 
@@ -125,7 +126,8 @@ object YTPlayerUtils {
                         connectivityManager,
                     ) ?: continue
                 streamUrl = findUrlOrNull(format, videoId) ?: continue
-                streamExpiresInSeconds = streamPlayerResponse.streamingData?.expiresInSeconds ?: continue
+                streamExpiresInSeconds =
+                    streamPlayerResponse.streamingData?.expiresInSeconds ?: continue
 
                 if (client.useWebPoTokens && webStreamingPot != null) {
                     streamUrl += "&pot=$webStreamingPot";
@@ -244,5 +246,17 @@ object YTPlayerUtils {
                 reportException(it)
             }
             .getOrNull()
+    }
+
+    /**
+     * Wrapper around the [PoTokenGenerator.getWebClientPoToken] function which reports exceptions
+     */
+    private fun getWebClientPoTokenOrNull(videoId: String): PoTokenResult? {
+        try {
+            return poTokenGenerator.getWebClientPoToken(videoId)
+        } catch (e: Exception) {
+            reportException(e)
+        }
+        return null
     }
 }