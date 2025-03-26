reocat/OuterTune

potoken: recreate generator & streaming pot if session id changed

app/src/main/java/com/dd3boh/outertune/utils/YTPlayerUtils.kt
@@ -12,8 +12,7 @@ import android.net.ConnectivityManager
 import androidx.media3.common.PlaybackException
 import com.dd3boh.outertune.constants.AudioQuality
 import com.dd3boh.outertune.db.entities.FormatEntity
-import com.dd3boh.outertune.utils.potoken.PoTokenGenerator
-import com.dd3boh.outertune.utils.potoken.PoTokenResult
+import com.dd3boh.outertune.utils.potoken.PoTokenException
 import com.zionhuang.innertube.NewPipeUtils
 import com.zionhuang.innertube.YouTube
 import com.zionhuang.innertube.models.YouTubeClient
@@ -78,7 +77,17 @@ object YTPlayerUtils {
          */
         val signatureTimestamp = getSignatureTimestampOrNull(videoId)
 
-        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId)?.let {
+        val isLoggedIn = YouTube.cookie != null
+        val sessionId =
+            if (isLoggedIn) {
+                // signed in sessions use dataSyncId as identifier
+                YouTube.dataSyncId
+            } else {
+                // signed out sessions use visitorData as identifier
+                YouTube.visitorData
+            }
+
+        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId, sessionId)?.let {
             Pair(it.playerRequestPoToken, it.streamingDataPoToken)
         } ?: Pair(null, null)
 
@@ -114,7 +123,7 @@ object YTPlayerUtils {
             if (client == MAIN_CLIENT) {
                 streamPlayerResponse = mainPlayerResponse
             } else {
-                if (client.loginRequired && YouTube.cookie == null) {
+                if (client.loginRequired && !isLoggedIn) {
                     // skip client if it requires login but user is not logged in
                     continue
                 }
@@ -260,9 +269,12 @@ object YTPlayerUtils {
     /**
      * Wrapper around the [PoTokenGenerator.getWebClientPoToken] function which reports exceptions
      */
-    private fun getWebClientPoTokenOrNull(videoId: String): PoTokenResult? {
+    private fun getWebClientPoTokenOrNull(videoId: String, sessionId: String?): PoTokenResult? {
         try {
-            return poTokenGenerator.getWebClientPoToken(videoId)
+            if (sessionId == null) {
+                throw PoTokenException("Session identifier is null")
+            }
+            return poTokenGenerator.getWebClientPoToken(videoId, sessionId)
         } catch (e: Exception) {
             reportException(e)
         }

rcky844/InnerTune

app/src/main/java/com/zionhuang/music/utils/YTPlayerUtils.kt
@@ -11,6 +11,7 @@ import com.zionhuang.innertube.models.YouTubeClient.Companion.WEB_REMIX
 import com.zionhuang.innertube.models.response.PlayerResponse
 import com.zionhuang.music.constants.AudioQuality
 import com.zionhuang.music.db.entities.FormatEntity
+import com.zionhuang.music.utils.potoken.PoTokenException
 import com.zionhuang.music.utils.potoken.PoTokenGenerator
 import com.zionhuang.music.utils.potoken.PoTokenResult
 import okhttp3.OkHttpClient
@@ -70,7 +71,17 @@ object YTPlayerUtils {
          */
         val signatureTimestamp = getSignatureTimestampOrNull(videoId)
 
-        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId)?.let {
+        val isLoggedIn = YouTube.cookie != null
+        val sessionId =
+            if (isLoggedIn) {
+                // signed in sessions use dataSyncId as identifier
+                YouTube.dataSyncId
+            } else {
+                // signed out sessions use visitorData as identifier
+                YouTube.visitorData
+            }
+
+        val (webPlayerPot, webStreamingPot) = getWebClientPoTokenOrNull(videoId, sessionId)?.let {
             Pair(it.playerRequestPoToken, it.streamingDataPoToken)
         } ?: Pair(null, null)
 
@@ -106,7 +117,7 @@ object YTPlayerUtils {
             if (client == MAIN_CLIENT) {
                 streamPlayerResponse = mainPlayerResponse
             } else {
-                if (client.loginRequired && YouTube.cookie == null) {
+                if (client.loginRequired && !isLoggedIn) {
                     // skip client if it requires login but user is not logged in
                     continue
                 }
@@ -251,9 +262,12 @@ object YTPlayerUtils {
     /**
      * Wrapper around the [PoTokenGenerator.getWebClientPoToken] function which reports exceptions
      */
-    private fun getWebClientPoTokenOrNull(videoId: String): PoTokenResult? {
+    private fun getWebClientPoTokenOrNull(videoId: String, sessionId: String?): PoTokenResult? {
         try {
-            return poTokenGenerator.getWebClientPoToken(videoId)
+            if (sessionId == null) {
+                throw PoTokenException("Session identifier is null")
+            }
+            return poTokenGenerator.getWebClientPoToken(videoId, sessionId)
         } catch (e: Exception) {
             reportException(e)
         }